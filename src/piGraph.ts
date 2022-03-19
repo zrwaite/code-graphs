005be2ae8203f0897a8d35cc449dfa598615265b
Zac Waite <129032699zw@gmail.com>
2022-03-19 07:28:19 -0400

    Restyled like standard github panels

--- src/piGraph.ts
+++ src/piGraph.ts
@@ -12,11 +12,13 @@ const createSlice = (width: number, height: number, ctx: any, start: number, end
 	let middle = {
 		x: width/2, y: height/2
 	}
-	let radius = Math.min(width, height)*0.48;
+	let radius = Math.min(width, height)*0.43;
 	let offset = 0; //radius*0.04;
 	let ox = Math.cos(median)*offset;
 	let oy = Math.sin(median)*offset;
 	ctx.fillStyle = colour;
+
+    ctx.strokeStyle = "transparent";
 	ctx.beginPath();
 	ctx.moveTo(middle.x + ox, middle.y + oy);
 	ctx.arc(middle.x+ox, middle.y+oy, radius, start+offset*0.0001, end-offset*0.0001);
@@ -46,17 +48,17 @@ const createPiGraph = async (ctx: any, languages: any[], width:number, height:nu
 }
 
 const createLegend = async (ctx: any, languages: any[], start: number, width:number, height:number) => {
-	ctx.fillStyle = "white";
-	ctx.fillRect(start, 0, width, height); 
-	ctx.fillStyle = "lightgrey";
-	ctx.fillRect(start+ width/20, height/40, width-width/10, height-height/8);
+	// ctx.fillStyle = "lightgrey";
+	// ctx.fillRect(start+ width/20, height/40, width-width/10, height-height/8);
 	ctx.textBaseline = "top";
 	ctx.textAlign = 'center';
 
 	ctx.font = 'bold 30pt Menlo';
-	ctx.fillStyle = "black";
+	ctx.fillStyle = "white";
 	ctx.fillText("Top Languages", start+width/2, height/30);
-	ctx.fillText("Since October", start+width/2, 2*height/20);
+	ctx.fillText("(By time spent)", start+width/2, 2*height/20);
+	ctx.font = 'bold 12pt Menlo';
+	ctx.fillText("Since October 2021*", start+width/2, 3.5*height/20);
 
 	ctx.font = 'bold 21pt Menlo';
 	ctx.fillStyle = "black";
@@ -64,25 +66,72 @@ const createLegend = async (ctx: any, languages: any[], start: number, width:num
 	for (let i=0; i<languages.length; i++) {
 		let language = languages[i];
 		ctx.fillStyle = language.colour;
-		ctx.fillRect(start+width/10, (i+4)*height/20, height/50, height/50);
-		ctx.fillText(language.name, start+2*width/10, (i+3.7)*height/20);
-		ctx.fillText(language.percent.toString() + "%", start+7*width/10, (i+3.7)*height/20);
+		ctx.fillRect(start+width/10, (i+4.5)*height/20, height/50, height/50);
+		ctx.fillText(language.name, start+2*width/10, (i+4.2)*height/20);
+		ctx.fillText(language.percent.toString() + "%", start+7*width/10, (i+4.2)*height/20);
 	}
 	ctx.font = 'bold 14pt Menlo';
-	ctx.fillStyle = "black";
+	ctx.fillStyle = "white";
 	ctx.textAlign = 'center';
 	let date = new Date().toLocaleDateString().toString();
-	ctx.fillText("Updated on: "+date, start+width/2, 18.4*height/20);
-	ctx.fillText("Charts coded by Zac in nodejs", start+width/2, 19*height/20);
+	ctx.fillText("Charts coded by Zac in nodejs", start+width/2, 18*height/20);
+	ctx.fillText("Updated on: "+date, start+width/2, 18.6*height/20);
+}
+
+const createBackground = (ctx: canvasClass.CanvasRenderingContext2D, width: number, height:number, borderRadius: number, borderWidth: number) => {
+	ctx.fillStyle = "black";
+    ctx.lineWidth = borderWidth;
+    ctx.strokeStyle = "white";
+	// ctx.arc(400, 400, 100, 0, 2 * Math.PI, false);
+	ctx.ellipse(borderRadius+borderWidth/2, borderRadius+borderWidth/2, borderRadius, borderRadius, 0, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.restore();
+	ctx.ellipse(width-borderRadius-borderWidth/2, borderRadius+borderWidth/2, borderRadius, borderRadius, 0, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.restore();
+	ctx.ellipse(borderRadius+borderWidth/2, height-borderRadius-borderWidth/2, borderRadius, borderRadius, 0, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.restore();
+	ctx.ellipse(width-borderRadius-borderWidth/2, height-borderRadius-borderWidth/2, borderRadius, borderRadius, 0, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.restore();
+	ctx.fill();
+
+	ctx.fillRect(borderRadius, borderWidth/2, width-2*borderRadius, height-2*borderWidth/2);
+	ctx.fillRect(borderWidth/2, borderRadius, width-2*borderWidth/2, height-2*borderRadius);
+	ctx.beginPath();
+	ctx.moveTo(0, borderRadius);
+	ctx.lineTo(0, height-borderRadius);
+	ctx.stroke(); 
+	ctx.beginPath();
+	ctx.moveTo(width, borderRadius);
+	ctx.lineTo(width, height-borderRadius);
+	ctx.stroke(); 
+	ctx.beginPath();
+	ctx.moveTo(borderRadius, 0);
+	ctx.lineTo(width-borderRadius, 0);
+	ctx.stroke(); 
+	ctx.beginPath();
+	ctx.moveTo(borderRadius, height);
+	ctx.lineTo(width-borderRadius, height);
+	ctx.stroke(); 
+	// ctx.fillStyle = "black";
+	// ctx.fill();
+    // ctx.lineWidth = 0;
+    // ctx.strokeStyle = "white";
+    // ctx.stroke();
 }
 
 const createImage = async () => {
 	let width = 1000;
 	let height = 650;
+	let borderRadius = 80;
+	let borderWidth = 12;
 	let canvas = createCanvas(width, height);
 	let ctx = canvas.getContext('2d');
 	let languages = await getLanguages();
-	createPiGraph(ctx, languages, width*0.6, height)
+	createBackground(ctx, width, height, borderRadius, borderWidth);
+	createPiGraph(ctx, languages, width*0.64, height)
 	createLegend(ctx, languages, width*0.6, width*0.4, height)
 	
 	const imageFolderPath = path.join(__dirname, "../images/");
