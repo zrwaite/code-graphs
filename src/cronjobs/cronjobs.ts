ec76dc8c564e2ae59ffbd1d1d97aa8f63530163b
Zac Waite <129032699zw@gmail.com>
2022-07-15 20:49:06 -0400

    Update data every hour instead of once a day

--- src/cronjobs/cronjobs.ts
+++ src/cronjobs/cronjobs.ts
@@ -2,7 +2,7 @@ import cron from "node-cron";
 import drawGraph from "../piGraph";
 const cronjobs = () => {
 	drawGraph();
-	cron.schedule('0 1 * * *', () => {
+	cron.schedule('0 * * * *', () => {
 		drawGraph();
 	});
 }
