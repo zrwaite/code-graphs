1ca822f6fa6862e0a40c91711718d9625a7e6036
Zac Waite <129032699zw@gmail.com>
2022-10-05 14:20:45 -0400

    Update ignore params

--- api/wakatime/getWakatimePi.go
+++ api/wakatime/getWakatimePi.go
@@ -12,11 +12,14 @@ func GetWakatimePiSVG(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Last-Modified", codeData.LastModified)
 	w.Header().Set("Expires", codeData.Expires)
 	ignoreString := r.URL.Query().Get("ignore")
-	var ignoreLanguages []string
-	if ignoreString == "" {
-		ignoreLanguages = []string{"JSON", "Markdown", "Other", "INI", "Text", "XML", "YAML", "Bash", "Git Config", "TOML", "Apache Config", "GitIgnore file", "Shell Script", "GraphQL", "Tex", "CMake", "Git"}
-	} else {
+	addDefaultIgnore := r.URL.Query().Get("addDefaultIgnore")
+	ignoreLanguages := []string{}
+	if ignoreString != "" {
 		ignoreLanguages = strings.Split(ignoreString, ",")
+	}
+	if addDefaultIgnore == "true" {
+		defaultIgnoreLanguages := []string{"JSON", "Markdown", "Other", "INI", "Text", "XML", "YAML", "Bash", "Git Config", "TOML", "Apache Config", "GitIgnore file", "Shell Script", "GraphQL", "Tex", "CMake", "Git"}
+		ignoreLanguages = append(ignoreLanguages, defaultIgnoreLanguages...)
 	}
 	languages := parseLanguages(codeData, ignoreLanguages)
 	s := svg.New(w)
