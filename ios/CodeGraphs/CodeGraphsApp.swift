d819921c35dd08339aa217778b7c90522057b0d7
Zac Waite <129032699zw@gmail.com>
2022-10-10 15:40:03 -0400

    IOS App initial commit

---
+++ ios/CodeGraphs/CodeGraphsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeGraphsApp.swift
+//  CodeGraphs
+//
+//  Created by Zachary Waite on 2022-10-10.
+//
+
+import SwiftUI
+
+@main
+struct CodeGraphsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/CodeGraphs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CodeGraphs
+//
+//  Created by Zachary Waite on 2022-10-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Code Graphs")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
