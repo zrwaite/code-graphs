30ae59af807b55222a041be12ee0ebd4958472eb
Zac Waite <129032699zw@gmail.com>
2022-12-09 01:04:18 -0500

    Try other redis connection again

--- web/db/redis.go
+++ web/db/redis.go
@@ -14,9 +14,9 @@ import (
 var Cache *redis.Client
 
 func ConnectToRedis() {
-	addr := "redis:6379"
+	addr := "redis:6380"
 	if config.CONFIG.Dev {
-		addr = "localhost:6379"
+		addr = "localhost:6380"
 	}
 	Cache = redis.NewClient(&redis.Options{
 		Addr:     addr,
